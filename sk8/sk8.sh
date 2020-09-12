2c230cd3f371d92fba6b5452f1bc01edef42cf73
John <john@hagstrand.com>
2020-09-12 18:34:12 +0700

    enable sk8 for boot loading

--- sk8/sk8.sh
+++ sk8/sk8.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-cd /home/john/webapps/robots/robots/webmon/ 
-python3 -u webmon.py &>> /var/log/skate/webmon.log
+
+# Copy the shell command to /usr/bin/ and make it executable.
+# $ sudo cp sk8.sh /usr/bin/
+# $ sudo chmod +x /usr/bin/sk8.sh
+# 
+# Copy the service file to /etc/systemd/system/ and set permissions.
+# $ sudo cp sk8.service /etc/systemd/system/
+# $ sudo chmod 644 /etc/systemd/system/sk8.service
+# 
+# Run the service.
+# $ sudo systemctl start sk8
+# $ sudo systemctl status sk8
+# $ sudo systemctl stop sk8
+# $ sudo systemctl enable sk8  # service will start at boot  
+
+cd /home/john/webapps/robots/robots/sk8/ 
+python3 -u sk8.py &>> /var/log/sk8.log
