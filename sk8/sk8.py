e7a25f47c9c04765348bea323e7fc15546fe1b12
John <john@hagstrand.com>
2020-09-12 18:04:26 +0700

    reorg and rename to sk8

---
+++ sk8/sk8.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.path.append('/home/john/webapps/robots/robots/skate/')
+sys.path.append('/home/john/webapps/robots/robots/webmon')
+sys.path.append('/home/john/webapps/robots/robots/eyes')
+
+#import importlib.util
+#spec = importlib.util.spec_from_file_location('webmon', '../webmon/webmon.py')
+#webmon = importlib.util.module_from_spec(spec)
+#spec.loader.exec_module(webmon)
+
+import webmon
+import eyes.py
+
+
