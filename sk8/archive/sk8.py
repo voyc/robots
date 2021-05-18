8dbe0302f759c62b492dcdd789d536137d1c955d
John Hagstrand <john@hagstrand.com>
2021-05-18 18:07:04 +0700

    fold latest code into sk8 folder

---
+++ sk8/archive/sk8.py
@@ -0,0 +1,14 @@
+# top level module for sk8
+
+import monad   # globals
+import cortex
+import portal
+import eyes
+import wheels
+
+monad.eyes = eyes.Eyes()   # fly the drone, recv video & telemetry, maintain map
+monad.wheels = wheels.Wheels()  # drive the skateboard via the arduino
+monad.cortex = cortex.Cortex()  # read map, plot route
+#monad.portal = portal.Portal()  # webserver, human interface
+
+monad.cortex.command('wakeup')
