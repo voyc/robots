a5aaa22e11437add9d98f3d250a8a60fde119580
John Hagstrand <john@hagstrand.com>
2024-01-22 09:56:45 +0700

    copy simRunningDiff to sim

--- sk8mini/awacs/detect/sim.py
+++ sk8mini/awacs/detect/sim.py
@@ -1,136 +1,141 @@
 '''
-sim.py - simulate realtime object detection
+simRunningDiff.py - simulte the camera, take a diff between each frame and the previous
 '''
 import cv2
 import numpy as np
 import os
 import argparse
+import copy
 
-import detect 
-import label as labl
+import detect
 import draw
-
-day_model = [
-{
-	"cls": 1,
-	"name": "cone",
-	"algo": 0,
-	"rotate": 0,
-	"values": [0, 86, 134, 255, 101, 196, 11, 26, 11, 26]
-},
-{
-	"cls": 2,
-	"name": "led",
-	"algo": 1,
-	"rotate": 0,
-	"values": [158, 5, 8, 5, 8]
-},
-{
-	"cls": 3,
-	"name": "sk8",
-	"algo": 0,
-	"rotate": 1,
-	"values": [31, 56, 48, 114, 22, 177, 35, 69, 59, 92]
-}
-]
-night_model = [
-{
-	"cls": 1,
-	"name": "cone",
-	"algo": 0,
-	"rotate": 0,
-	"values": [0, 69, 108, 156, 77, 148, 14, 40, 15, 40] 
-},
-{
-	"cls": 2,
-	"name": "led",
-	"algo": 1,
-	"rotate": 0,
-	"values": [158, 5, 8, 5, 8] 
-},
-{
-	"cls": 3,
-	"name": "sk8",
-	"algo": 0,
-	"rotate": 1,
-	"values": [0, 63, 33, 58, 22, 177, 7, 68, 44, 152] 
-}
-]
+import frame as frm 
+import score
+import label as labl
 
 gargs = None
+gframendx = 0
+gframelist = []
 
-def main():
-	global gargs
-	idir = 'photos/training/'
-	idir = 'photos/20231216-092941/'  # day
-	idir = 'photos/20240109-174051/'  # night
+def getFrame():
+	global gframendx, gframelist
+	if gframendx == 0:
+		gframelist = frm.getFrameList(gargs.idir)
+	if gframendx >= len(gframelist):
+		return None,0
+	fnum = gframelist[gframendx]
+	frame = cv2.imread(frm.fqjoin(gargs.idir, fnum, gargs.iext), cv2.IMREAD_UNCHANGED)
+	gframendx += 1
+	return frame,fnum
 
-	# get command-line parameters 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-id' ,'--idir'    ,default=idir        ,help='input folder'        )
-	gargs = parser.parse_args()	# returns Namespace object, use dot-notation
+def diffFrames(current,previous):
+	diff = cv2.absdiff(current, previous)
 
-	# loop jpgs
-	image_folder = gargs.idir
-	jlist = detect.getFrameList(image_folder)
-	pause = False
-	ndx = 0
-	lastframe = len(jlist)-1
-	model = None
-	while ndx < lastframe:
-		# open jpg
-		fnum = jlist[ndx]
-		fqname = os.path.join(image_folder, fnum + '.jpg')
-		img = cv2.imread(fqname, cv2.IMREAD_UNCHANGED)
+	imgDiff1 = cv2.absdiff(current, previous)	
+	imgDiff2 = cv2.absdiff(previous, imgDiff1)
+	imgDiff3 = cv2.absdiff(current, imgDiff2)
 
-		# first-time, choose model
-		if ndx == 0:
-			mean = detect.averageBrightness(img)
-			if mean > 110:
-				print('using day model')
-				model = day_model
-			else:
-				print('using night model')
-				model = night_model
+	imgDiff2a = cv2.absdiff(current, imgDiff1)
+	imgDiff4 = cv2.absdiff(previous, imgDiff2a)
 
-		# detect objectts
-		labels = detect.detectObjects(img,model)
-		slabels = labl.format(labels, 'realtime')
-		print(slabels)
+	#draw.showImage(previousframe, frame, imgDiff1, imgDiff2, imgDiff2a, imgDiff3, imgDiff4)
+	return imgDiff4
 
-		# display jpg	
-		img = draw.drawImage(img,labels, {'format':'sbs'})
-		cv2.imshow(f'sim {gargs.idir}', img)
+def labelsFromMask(mask, cls, dim, maxcount):
+	labels = []
+	cnts,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	armse = []
+	for cnt in cnts:
+		rect = cv2.minAreaRect(cnt) 
+		size = rect[1]
+		rmse = score.rmse(dim, size)
+		armse.append(rmse)
+		label = labl.labelFromRect(cls, rect, which=False, score=rmse)
+		labels.append(label)
 
-		# animate and allow manual interaction
-		key = cv2.waitKey(200)
-		if key & 0xFF == ord('q'):	# quit
-			break
-		elif key & 0xFF == 13:		# return, pause
-			pause = not pause
-		elif key & 0xFF == ord('n'):	# next
-			ndx += 1
-			if ndx > lastframe:
-				ndx = 0
-		elif key & 0xFF == ord('p'):	# prev
-			ndx -= 1
-			if ndx < 0:
-				ndx = lastframe
+	# sort the labels ascending by error
+	sortedlabels = sorted(labels, key=lambda a: a[labl.scr])
 
-		if not pause:
-			ndx += 1
+	# take the n with lowest score
+	bestlabels = sortedlabels[0:maxcount]
+
+	# convert error to probability
+	maxerror = max(armse)
+	for label in bestlabels:
+		rmse = label[labl.scr]
+		prob = score.probability(rmse, maxerror)
+		label[labl.scr] = prob
+	return bestlabels
+
+def preprocessMask(mask):
+	kernelsize = 3
+	dilateiterations = 3
+	kernel = np.ones((kernelsize, kernelsize))
+	mask = cv2.dilate(mask, kernel, iterations=dilateiterations)
+	#mask = cv2.erode(mask, kernel, iterations=dilateiterations)
+	return mask
+
+def looper():
+	previousframe,_ = getFrame()
+	diffs = []
+	masks = []
+	labelsets = []
+	aframes = []
+	framecnt = 0
+	
+	while True:
+		frame,fnum = getFrame()
+		if frame is None:
+			break;
+		framecnt += 1
+
+		diff = diffFrames(frame, previousframe)
+		diffs.append(diff)
+
+		gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+		t, mask = cv2.threshold(gray, 0,  255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+		mask = preprocessMask(mask)
+		masks.append(mask)
+
+		labels = labelsFromMask(mask, 2, (50,70), 1)
+		labelsets.append(labels)
+		if gargs.olabelsufx:
+			labl.write(labels,frm.fqjoin(gargs.idir, fnum, gargs.olabelsufx))
+
+		aframe = draw.drawImage(frame,labels,options={"title":fnum})
+		aframes.append(aframe)
+
+		if gargs.nthshow > 0 and (framecnt) % gargs.nthshow  == 0:
+			key = draw.showImage(aframes, fps=gargs.fps)
+			aframes = []
+			#draw.showImage(diffs+masks+aframes, cols=len(aframes))
+			if key & 0xFF == ord('q'):	# quit
+				break
+			elif key & 0xFF == ord('n'):	# next
+				pass
+
+		previousframe = frame
 
 	cv2.destroyAllWindows()
 
+def main():
+	global gargs
+	defidir = 'photos/20231216-092941/'  # day
+	defiext = 'jpg'
+	defnthshow = 0
+	deffps = 0
+	defolabelsufx = 'label.csv'
 
-	#labels = []
-	#for m in range(2):
-	#	label = detectObjects(img, model, m)
-	#	labels += label
+	# get command-line parameters 
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-id' ,'--idir'                ,default=defidir      ,help='input folder'        )
+	parser.add_argument('-ie' ,'--iext'                ,default=defiext      ,help='input extension'     )
+	parser.add_argument('-ns' ,'--nthshow'   ,type=int ,default=defnthshow   ,help='stop and refresh UI every nth frame'   )
+	parser.add_argument('-fps','--fps'       ,type=int ,default=deffps       ,help='fps, when nthshow is 0'   )
+	parser.add_argument('-ol' ,'--olabelsufx'          ,default=defolabelsufx,help='suffix of output label file'   )
+	gargs = parser.parse_args()	# returns Namespace object, use dot-notation
 
-	#print(labels)
-
-
+	looper()
 
 if __name__ == "__main__":
 	main()
---
+++ sk8mini/awacs/detect/simOrg.py
@@ -0,0 +1,136 @@
+'''
+sim.py - simulate realtime object detection
+'''
+import cv2
+import numpy as np
+import os
+import argparse
+
+import detect 
+import label as labl
+import draw
+
+day_model = [
+{
+	"cls": 1,
+	"name": "cone",
+	"algo": 0,
+	"rotate": 0,
+	"values": [0, 86, 134, 255, 101, 196, 11, 26, 11, 26]
+},
+{
+	"cls": 2,
+	"name": "led",
+	"algo": 1,
+	"rotate": 0,
+	"values": [158, 5, 8, 5, 8]
+},
+{
+	"cls": 3,
+	"name": "sk8",
+	"algo": 0,
+	"rotate": 1,
+	"values": [31, 56, 48, 114, 22, 177, 35, 69, 59, 92]
+}
+]
+night_model = [
+{
+	"cls": 1,
+	"name": "cone",
+	"algo": 0,
+	"rotate": 0,
+	"values": [0, 69, 108, 156, 77, 148, 14, 40, 15, 40] 
+},
+{
+	"cls": 2,
+	"name": "led",
+	"algo": 1,
+	"rotate": 0,
+	"values": [158, 5, 8, 5, 8] 
+},
+{
+	"cls": 3,
+	"name": "sk8",
+	"algo": 0,
+	"rotate": 1,
+	"values": [0, 63, 33, 58, 22, 177, 7, 68, 44, 152] 
+}
+]
+
+gargs = None
+
+def main():
+	global gargs
+	idir = 'photos/training/'
+	idir = 'photos/20231216-092941/'  # day
+	idir = 'photos/20240109-174051/'  # night
+
+	# get command-line parameters 
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-id' ,'--idir'    ,default=idir        ,help='input folder'        )
+	gargs = parser.parse_args()	# returns Namespace object, use dot-notation
+
+	# loop jpgs
+	image_folder = gargs.idir
+	jlist = detect.getFrameList(image_folder)
+	pause = False
+	ndx = 0
+	lastframe = len(jlist)-1
+	model = None
+	while ndx < lastframe:
+		# open jpg
+		fnum = jlist[ndx]
+		fqname = os.path.join(image_folder, fnum + '.jpg')
+		img = cv2.imread(fqname, cv2.IMREAD_UNCHANGED)
+
+		# first-time, choose model
+		if ndx == 0:
+			mean = detect.averageBrightness(img)
+			if mean > 110:
+				print('using day model')
+				model = day_model
+			else:
+				print('using night model')
+				model = night_model
+
+		# detect objectts
+		labels = detect.detectObjects(img,model)
+		slabels = labl.format(labels, 'realtime')
+		print(slabels)
+
+		# display jpg	
+		img = draw.drawImage(img,labels, {'format':'sbs'})
+		cv2.imshow(f'sim {gargs.idir}', img)
+
+		# animate and allow manual interaction
+		key = cv2.waitKey(200)
+		if key & 0xFF == ord('q'):	# quit
+			break
+		elif key & 0xFF == 13:		# return, pause
+			pause = not pause
+		elif key & 0xFF == ord('n'):	# next
+			ndx += 1
+			if ndx > lastframe:
+				ndx = 0
+		elif key & 0xFF == ord('p'):	# prev
+			ndx -= 1
+			if ndx < 0:
+				ndx = lastframe
+
+		if not pause:
+			ndx += 1
+
+	cv2.destroyAllWindows()
+
+
+	#labels = []
+	#for m in range(2):
+	#	label = detectObjects(img, model, m)
+	#	labels += label
+
+	#print(labels)
+
+
+
+if __name__ == "__main__":
+	main()
